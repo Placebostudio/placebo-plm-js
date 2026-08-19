1b9da2e9db25431e6fbc0bdde654dc813112a831
Roei <Roeit1977@gmail.com>
2026-08-19 14:33:42 +0300

    fixing image access for display

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'eiualxzcgjdfqkthsdce.supabase.co',
+                pathname: '/storage/v1/object/public/**',
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig;
